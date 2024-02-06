4961f717eca81acf1d5744a858fd3b596a49d53c
Xirios <80783223+GabrielMnss@users.noreply.github.com>
2024-02-06 13:24:38 +0100

    Update SQL_connect.py

--- SQL_connect.py
+++ SQL_connect.py
@@ -6,7 +6,7 @@ import pymysql
 connection = mysql.connector.connect(host='192.168.0.29',
                                      database='IUT_DB',
                                      user='root',
-                                     password='gabrielmassoN01612!')
+                                     password='none')
 connection.autocommit = True
 engine = create_engine("mysql+mysqldb://root:gabrielmassoN01612!@192.168.0.29/IUT_DB")
 
